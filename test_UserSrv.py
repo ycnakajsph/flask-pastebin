43e1e19d3d10cf8897a6813ac44333a2dcff8efd
Yann CARDAILLAC <yann.cardaillac@gmail.com>
2022-01-09 20:16:45 +0100

    add subprocess within tests

--- test_UserSrv.py
+++ test_UserSrv.py
@@ -1,13 +1,27 @@
 import unittest
 import requests
 import shlex
+import subprocess
+import time
 
 class TestUserSrv(unittest.TestCase):
+
+	SrvSubprocess = None
 
 	TestPort = "99"
 	SrvAddr = "127.0.0.1"
 	SrvUrl = "http://" + SrvAddr + ":" + TestPort
 
+	def setUp(self):
+		cmd = "python UserSrv.py --port="+self.TestPort
+		args = shlex.split(cmd)
+		self.SrvSubprocess  = subprocess.Popen(args) # launch command as a subprocess
+		time.sleep(3)
+
+	def tearDown(self):
+		print("killing subprocess UserSrv")
+		self.SrvSubprocess.kill()
+		self.SrvSubprocess.wait()
 
 	def test_launchSrv(self):
 		response = requests.get(self.SrvUrl+"/isalive")
