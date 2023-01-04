3f7f99b39dc1ad6d15a17dfc0b0ac3f952e48939
Yann CARDAILLAC <yann.cardaillac@eolane.com>
2023-01-04 10:30:31 +0100

    add early test for login

--- test_user_server.py
+++ test_user_server.py
@@ -8,7 +8,7 @@ class TestUserSrv(unittest.TestCase):
 
 	SrvSubprocess = None
 
-	TestPort = "99"
+	TestPort = "9009"
 	SrvAddr = "127.0.0.1"
 	SrvUrl = "http://" + SrvAddr + ":" + TestPort
 
@@ -26,6 +26,15 @@ class TestUserSrv(unittest.TestCase):
 	def test_launchSrv(self):
 		response = requests.get(self.SrvUrl+"/isalive")
 		self.assertEqual(response.status_code,200)
+
+	def test_login(self):
+		response = requests.post(self.SrvUrl+"/login")
+		self.assertEqual(response.status_code,400) #missing json payload
+
+		response = requests.post(self.SrvUrl+"/login",json={"key": "value"})
+		self.assertEqual(response.status_code,200) # we accept anything at this stage
+
+
 
 if __name__ == '__main__':
 	unittest.main()
