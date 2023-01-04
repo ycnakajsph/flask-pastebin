d7cb369f2ca92e4c3148a4ddfd758ce770adb4e1
Yann CARDAILLAC <yann.cardaillac@eolane.com>
2023-01-04 12:37:46 +0100

    add less dummy user login validation

--- user_server.py
+++ user_server.py
@@ -13,25 +13,37 @@ from docopt import docopt
 from flask import Flask
 from flask import Response
 from flask import request
-
+from flask import jsonify
+from flask_json_schema import JsonSchema, JsonValidationError
 
 APP = Flask(__name__)
+schema = JsonSchema(APP)
 
-# can be tested with :
-# $ curl -X GET 127.0.0.1:<port>/isalive
+@APP.errorhandler(JsonValidationError)
+def validation_error(e):
+    return jsonify({ 'error': e.message, 'errors': [validation_error.message for validation_error  in e.errors]}), 400
+
 @APP.route('/isalive', methods=['GET'])
 def is_alive():
 	return Response(status=200)
 
-# can be tested with:
-# $ curl -v -X POST 127.0.0.1:9009/login -H "Content-Type: application/json"  -d '{"key1":"value1", "key2":"value2"}'
+# of course schemas shall go in a separated folder
+login_schema = {
+	"type" : "object",
+	"required" : ["username","password"],
+	"properties" : {
+		"username" : {"type" : "string"},
+		"password" : {"type" : "string"},
+	},
+}
+
 @APP.route('/login', methods=['POST'])
+@schema.validate(login_schema)
 def login():
 	json_payload = request.json
 	if json_payload is not None:
 		print(json_payload)
 		return Response(status=200)
-
 	return Response(status=400)
 
 if __name__ == '__main__':
